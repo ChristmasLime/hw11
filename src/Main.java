ce3fd7795c2a31a97fa73fee245b92485236fb7f
ChristmasLime <christmasboom@mail.ru>
2023-03-03 01:47:00 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,60 @@
 public class Main {
-    public static void main(String[] args) {
+public static void years(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год вискокосный");
+        } else {
+            System.out.println(year + " год не високосный");
+        }
+    }
+public static void clientOS(int version) {
+        if (version == 1) {
+            System.out.println("Android");
+        } else if (version == 0) {
+            System.out.println("iOS");
+        } else  {
+            System.out.println("Версия неопознана");
+        }
+    }
+public static void ageDevice(int age) {
+        if (age > 2015) {
+            System.out.println("Установите обычную версию ");
+        } else {
+            System.out.println("Установите облегченную версию ");
+        }
+    }
+public static void deliveryDistance(int km) {
+
+        if (km == 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (km >= 20 && km <= 60) {
+            System.out.println("Потребуется дней: " + 2);
+        } else if (km > 60 && km <= 100) {
+            System.out.println("Потребуется дней: " + 3);
+        } else if (km > 100 || km < 20) {
+            System.out.println("Доставки нет");
+        }
+    }
+
+public static void main(String[] args) {
         task1();
         task2();
         task3();
-        task4();
-        task5();
-        task6();
-        task7();
     }
 
-    public static void task1() {
+public static void task1() {
         System.out.println("Задача 1");
-
+        years(2001);
     }
 
-    public static void task2() {
+public static void task2() {
         System.out.println("Задача 2");
-
+        ageDevice(2000);
+        clientOS(0);
     }
 
-    public static void task3() {
+public static void task3() {
         System.out.println("Задача 3");
-
-    }
-
-    public static void task4() {
-        System.out.println("Задача 4");
-
-    }
-
-    public static void task5() {
-        System.out.println("Задача 5");
-
-    }
-
-    public static void task6() {
-        System.out.println("Задача 6");
-
-    }
-
-    public static void task7() {
-        System.out.println("Задача 7");
-
+        deliveryDistance(45);
     }
 }
+
